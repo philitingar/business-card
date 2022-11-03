49bcbdee199f86b9fbdd4dfafda31e5a51119d45
Timi <timi@Timis-Air.lan>
2022-11-03 15:23:39 +0100

    first commit

---
+++ TimeaCarrd/TimeaCarrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeaCarrdApp.swift
+//  TimeaCarrd
+//
+//  Created by Timi on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeaCarrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
