3698cc7efca1d29940fd36a5b8c337b13aaedb88
Timea B <timea.bartha.93@gmail.com>
2025-02-25 15:15:01 +0100

    New Changes To the Meowcard

---
+++ TimeaCarrd/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  TimeaCarrd
+//
+//  Created by Timea Bartha on 25/2/25.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let lightTeal = Color(red: 0.09, green: 0.63, blue: 0.52)
+}
